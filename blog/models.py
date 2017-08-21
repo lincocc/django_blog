7d05e10758e30789a2082547fa5920c0b777b9d1
cc <cc@example.com>
2017-08-21 10:18:29 +0800

    add models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,28 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.contrib.auth.models import User
+from django.db import models
+from django.utils import timezone
+
+
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published', default=timezone.now)
+    summary = models.TextField()
+    content = models.TextField()
+    user = models.ForeignKey(User)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    content = models.TextField()
+    user = models.ForeignKey(User)
+    post = models.ForeignKey(Post)
+    pub_date = models.DateTimeField('date published', default=timezone.now)
+
+    def __str__(self):
+        return self.content
