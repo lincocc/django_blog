a679414c1ae1a530fb23e4a14e0f52e8db0191f6
cc <cc@example.com>
2017-08-21 11:04:10 +0800

    add Post & Comment at admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from blog.models import Post, Comment
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_per_page = 30
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment)
