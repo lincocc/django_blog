d96ed61c753894820df687bb6d1c544c853e50f1
cc <cc@example.com>
2017-08-25 13:48:21 +0800

    use router

--- blog/urls.py
+++ blog/urls.py
@@ -1,38 +1,17 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
+from rest_framework.routers import DefaultRouter
 from rest_framework.urlpatterns import format_suffix_patterns
 
 from blog.views import PostViewSet, UserViewSet
 from . import views
 
+router = DefaultRouter()
+router.register(r'posts', PostViewSet)
+router.register(r'users', UserViewSet)
+
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<post_id>[0-9a-fA-F-]+)/$', views.detail, name='detail'),
     url(r'^edit/(?P<post_id>[0-9a-fA-F-]+)/$', views.edit, name='edit'),
+    url(r'^api/', include(router.urls))
 ]
-
-post_list = PostViewSet.as_view({
-    'get': 'list',
-    'post': 'create'
-})
-
-post_detail = PostViewSet.as_view({
-    'get': 'retrieve',
-    'put': 'update',
-    'patch': 'partial_update',
-    'delete': 'destroy'
-})
-
-user_list = UserViewSet.as_view({
-    'get': 'list'
-})
-
-user_detail = UserViewSet.as_view({
-    'get': 'retrieve'
-})
-urlpatterns += format_suffix_patterns([
-    url(r'^api/$', views.api_root),
-    url(r'^api/posts/$', post_list, name='post-list'),
-    url(r'^api/posts/(?P<pk>[0-9a-fA-F-]+)/$', post_detail, name='post-detail'),
-    url(r'^api/users/$', user_list, name='user-list'),
-    url(r'^api/users/(?P<pk>[0-9]+)/$', user_detail, name='user-detail'),
-])
