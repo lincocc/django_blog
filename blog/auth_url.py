ee48a66546b0e516c49f1deea2d8e6a9449c456c
cc <cc@example.com>
2017-09-11 18:24:41 +0800

    refactor login page 1

--- blog/auth_url.py
+++ blog/auth_url.py
@@ -3,7 +3,7 @@ from django.contrib.auth import views as auth_views
 from .views import register
 
 urlpatterns = [
-    url(r'^login/$', auth_views.LoginView.as_view(template_name="blog/auth/login.html"), name='login'),
+    url(r'^login/$', auth_views.LoginView.as_view(template_name="blog/auth/login1.html"), name='login'),
     url(r'^logout/$', auth_views.LogoutView.as_view(template_name="blog/auth/logout.html"), name='logout'),
     # url('^', include('django.contrib.auth.urls')),
     url(r'^register/$', register, name='register')
