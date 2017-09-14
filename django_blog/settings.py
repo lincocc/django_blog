7b9be1d4c450d170c1d3ee53c462fbe5e97c966c
cc <cc@example.com>
2017-09-14 18:16:46 +0800

    change to language cn

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -99,9 +99,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
-
+# LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-Hans'
 TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
@@ -112,7 +111,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'blog', 'static')
 # TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'templates'),)
